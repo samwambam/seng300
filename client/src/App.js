ef67b817ed7c47354fefc4e3ec151ba2d6e09bf4
Richard Williams <richard.lluttrell@gmail.com>
2020-03-06 13:19:54 -0700

    incorporated Ryan's  Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">University of Manitoba Scholarships</h1>
-        </header>
-        <p className="App-intro">
-         Hello, World!
-        </p>
+      <div class = "logo"></div>
+      <div class = "login box"> {/*<!-- Div section for the login box -->*/}
+        <header>Welcome to UWinnipeg Scholarships</header> {/*<!-- Header for the webpage-->*/}
+        <form class = "login box" method = "POST"> {/*Form for the login box, calls method to send information to backend*/}
+          <div>{/*Section for the ID text field*/}
+          <input type = "text" placeholder = "ID" name = "ID" class = "id"></input>
+          </div>
+          <div>{/*Section for the password text field*/}
+          <input type = "Password" placeholder = "Password" name = "password" class = "pword"></input>
+          </div>
+          <div>{/*Section for the submit button*/}
+          <input type = "submit" value = "Login"></input>
+          </div>
+        </form>
       </div>
+  </div>
     );
   }
 }
