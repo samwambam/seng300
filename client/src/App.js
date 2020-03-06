111869fee3f212abbf974ada64ec91f841e15d7c
Richard Williams <richard.lluttrell@gmail.com>
2020-03-06 13:10:35 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">University of Manitoba Scholarships</h1>
+        </header>
+        <p className="App-intro">
+         Hello, World!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
