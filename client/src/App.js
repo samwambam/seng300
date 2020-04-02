65c63c08f915dd4fd7582483e7a61dd3410f8e1b
samwambam <sammy.tran1@ucalgary.ca>
2020-04-02 12:49:13 -0600

    removed the unneeded lines
    some lines were left in when I tried to solve merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,6 @@ import Login from "./Login";
 
 class App extends Component {
     render() {
-      
-      /*bad area below D:*/
-      return (
-        <Router> 
-            <Switch>
-            <Route path='/login' exact component = {Login} /> 
-            <Route path='/portal' component = {Portal} /> 
-
     /*bad area below D:*/
         return (
             <Router> 
